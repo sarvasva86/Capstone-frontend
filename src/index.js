9e07ace8dfa78c4d61cee0b3b3a5da1c04c9b46b
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 17:16:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import "./styles/global.css";
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
+
+reportWebVitals();
