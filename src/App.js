1321aeaf83f1c13c07e78539cd92c0b5fbb2f679
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 16:45:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import ItineraryPage from "./pages/ItineraryPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
