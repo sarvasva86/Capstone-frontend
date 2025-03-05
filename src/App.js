6ccf73074ef3c1b259380da16fbd511fa63c5436
sarvasva86 <suchipatel5486@gmail.com>
2025-03-05 17:05:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import ItineraryPage from './pages/ItineraryPage'; // Add this import
 import NotFound from './pages/NotFound';
 
 function App() {
