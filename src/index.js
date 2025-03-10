9363034e49b233cff7a30dee714f3062249d91cb
sarvasva86 <suchipatel5486@gmail.com>
2025-03-10 13:34:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ItineraryProvider } from './contexts/ItineraryContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ItineraryProvider>
+      <App />
+    </ItineraryProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
