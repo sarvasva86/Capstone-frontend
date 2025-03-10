3ce61785da82cac767f2cac05752a7589c22d8e0
sarvasva86 <suchipatel5486@gmail.com>
2025-03-10 13:29:35 -0400

    Create client.js

---
+++ src/api/client.js
@@ -0,0 +1,26 @@
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Request-ID': uuidv4() // For request tracing
+  }
+});
+
+// Add request interceptor
+axiosClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// Add response interceptor
+axiosClient.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      window.location.href = '/login?session_expired=true';
+    }
+    return Promise.reject(error);
+  }
+);
