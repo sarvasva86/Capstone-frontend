ac24abf8e4efdcd1d3d1379f6a463c7d54acd51c
sarvasva86 <suchipatel5486@gmail.com>
2025-03-12 10:51:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import App from "./App";
+import { ItineraryProvider } from "./contexts/ItineraryContext";
 
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ItineraryProvider } from './contexts/ItineraryContext';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(
-  <React.StrictMode>
-    <ItineraryProvider> {/* ✅ Wrap your app inside the provider */}
-      <App />
-    </ItineraryProvider>
-  </React.StrictMode>
-);
-
-ReactDOM.render(
   <React.StrictMode>
     <ItineraryProvider>
       <App />
     </ItineraryProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
